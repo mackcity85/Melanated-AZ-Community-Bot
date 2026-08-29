93af3b0ad4212208d60b34e34448603c1f388a55
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-08-29 03:13:26 -0700

    Initialize games package with documentation
    Add module documentation for the Game Center package.

---
+++ games/__init__.py
@@ -0,0 +1,13 @@
+# ==========================================================
+# Melanated AZ Bot
+# games/__init__.py
+#
+# Melanated AZ Game Center
+# ==========================================================
+
+"""
+Melanated AZ Game Center.
+
+This package contains all Game Center
+categories and game modules.
+"""
