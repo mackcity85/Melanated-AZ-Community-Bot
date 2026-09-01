13584e809ae60413d39a02093eb9711d8f6d8e5b
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-08-31 20:09:45 -0700

    Add game categories and titles to registry.py

---
+++ real_games/registry.py
@@ -0,0 +1,35 @@
+ARCADE
+  Snake
+  Pac-Man
+  Pong
+  Breakout
+  Flappy
+  Crossy Road
+  Bubble Pop
+  etc.
+
+BOARD
+  Monopoly
+  Chess
+  Checkers
+  Risk
+  Battleship
+  Yahtzee
+  Connect Four
+  Ludo
+  etc.
+
+SHOOTING
+  Alien Battle
+  Fighter Jet
+  Gunman
+  Gun Run
+  etc.
+
+SPORTS
+  Basketball
+  Football
+  Cricket
+  Bowling
+  Archer
+  etc.
