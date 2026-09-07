0393d55e20961de2d41cd7d75b2af28c605e2089
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-09-07 14:22:57 -0700

    Import handle_real_game_deep_link directly

--- real_games/__init__.py
+++ real_games/__init__.py
@@ -3,11 +3,7 @@ Melanated AZ Real Games
 """
 
 from .real_games import real_games_bp
-
-try:
-    from .deep_links import handle_real_game_deep_link
-except ImportError:
-    handle_real_game_deep_link = None
+from .deep_links import handle_real_game_deep_link
 
 __all__ = [
     "real_games_bp",
