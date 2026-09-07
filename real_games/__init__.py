0bf858eb492bdbf29fa334282975ba075e1f9de3
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-09-07 14:13:40 -0700

    Update __init__.py

--- real_games/__init__.py
+++ real_games/__init__.py
@@ -1,10 +1,11 @@
-# ==========================================================
-# Melanated AZ Real Games
-# ==========================================================
+"""
+Melanated AZ Real Games
+"""
 
 from .real_games import real_games_bp
-
+from .deep_links import handle_real_game_deep_link
 
 __all__ = [
-    "real_games_bp"
+    "real_games_bp",
+    "handle_real_game_deep_link",
 ]
