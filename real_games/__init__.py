7f358bb330bf5771da4bb6c5b9ed419a2d79130d
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-09-07 14:53:14 -0700

    Update __all__ to exclude handle_real_game_deep_link
    Removed handle_real_game_deep_link from exports.

--- real_games/__init__.py
+++ real_games/__init__.py
@@ -3,9 +3,7 @@ Melanated AZ Real Games
 """
 
 from .real_games import real_games_bp
-from .deep_links import handle_real_game_deep_link
 
 __all__ = [
     "real_games_bp",
-    "handle_real_game_deep_link",
 ]
