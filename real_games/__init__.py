566d88cfb3c88b9b8ec47ec7fe1ba2ec532af5d8
mackcity85 <52261525+mackcity85@users.noreply.github.com>
2026-09-07 14:19:52 -0700

    Refactor imports and handle ImportError for deep_links

--- real_games/__init__.py
+++ real_games/__init__.py
@@ -2,7 +2,12 @@
 Melanated AZ Real Games
 """
 
-from .deep_links import handle_real_game_deep_link
+from .real_games import real_games_bp
+
+try:
+    from .deep_links import handle_real_game_deep_link
+except ImportError:
+    handle_real_game_deep_link = None
 
 __all__ = [
     "real_games_bp",
